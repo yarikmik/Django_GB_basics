6247acae046815827decae7f6a3af6347fae91ef
root <root@80-78-240-57.cloudvps.regruhosting.ru>
2021-09-04 10:22:45 +0300

    fix debug_toolbar

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -192,6 +192,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'geekshop', 'static'),
+)
+
 STATICFILES_FINDERS = [  # описывает где джанго ищет файлики statics
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
